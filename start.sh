59997a8b128795a25680106bb54f8aefd8c821be
Hassan-abdi-dev <78468520+Hassan-abdi-dev@users.noreply.github.com>
2021-06-14 12:58:08 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-export NEWS_API_KEY='febac0cde7aa4532b506538a833c1805'
-export SECRET_KEY='TUSIZOEANE'
+export NEWS_API_KEY='ba5b53ba848f91d961181876aa8f79ee55f2a4a2'
+export SECRET_KEY='newsflah001'
 python3 manage.py server
